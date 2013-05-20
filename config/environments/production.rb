1482394e46f891d01ac7fdf25b1a22597bc63e45
aranair <boa.homan@otcanalytics.com>
2013-05-20 22:46:21 +0800

    stop asset compilation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,8 @@ Blog::Application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
+  config.assets.enabled = false 
+
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
